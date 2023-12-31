8c97b53f04b803604d428df3582f5e9c045a96be
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-12-31 02:09:43 -1000

    Add Tailwind styles to signin page

--- src/components/SigninForm.svelte
+++ src/components/SigninForm.svelte
@@ -30,22 +30,37 @@
   }
 </script>
 
-<form on:submit={submitForm}>
-  <h2>Sign in</h2>
+<form on:submit={submitForm} class="flex flex-col w-8/12">
+  <h2 class="font-semibold text-4xl mb-12">Sign in</h2>
 
-  <fieldset>
-    <label for="email">
-      <span>Email</span>
-      <input type="text" placeholder="Your email" bind:value={email} />
-    </label>
-  </fieldset>
+  <div class="flex flex-col gap-8 px-10 py-12 rounded border border-white/10">
+    <fieldset>
+      <label for="email" class="flex flex-col gap-2">
+        <span>Email</span>
+        <input
+          type="text"
+          bind:value={email}
+          class="text-zinc-900 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-emerald-400/80 duration-150"
+          required
+        />
+      </label>
+    </fieldset>
 
-  <fieldset>
-    <label for="password">
-      <span>Your password</span>
-      <input type="password" bind:value={password} />
-    </label>
-  </fieldset>
+    <fieldset>
+      <label for="password" class="flex flex-col gap-2">
+        <span>Password</span>
+        <input
+          type="password"
+          bind:value={password}
+          class="text-zinc-900 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-emerald-400/80 duration-150"
+          required
+        />
+      </label>
+    </fieldset>
 
-  <button>Sign in</button>
+    <button
+      class="border border-white/10 rounded bg-emerald-900 px-3 py-2 mt-6 hover:bg-emerald-800 focus:bg-emerald-800 focus:outline-none focus:ring focus:ring-emerald-400/80 duration-150"
+      >Sign in</button
+    >
+  </div>
 </form>
