c71baab63005d296d66eae866053b741de9cf367
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-12-31 00:40:28 -1000

    Add types for Vite environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  readonly APPWRITE_ENDPOINT: string;
+  readonly APPWRITE_PROJECT_ID: string;
+}
