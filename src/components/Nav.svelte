7ad37e7b29ee08430ba9224700b82a0343a58500
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2024-01-03 22:35:06 -1000

    Add navbar as Nav Svelte component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,6 @@
+<nav class="flex justify-between px-8 py-6 border-b border-white/10">
+  <span class="text-lg font-bold"> Budget Keeper</span>
+  <li class="flex justify-between gap-6">
+    <button on:click={() => alert("BANG!")}>Sign Out</button>
+  </li>
+</nav>
